0de22c18e3a7c5cebc0960dbc28872db2fd0c128
ilijasvn <ilijasvn@94682870-0719-0410-b6ce-bec82ef251d7>
2007-01-21 00:24:25 +0000

    * Doctrine connect method is added to engines init() method (commented) with validation turned to on by default.  * Mass-set method for Doctrine record is added (through Angie_Doctrine_Record) with support for allowed and protected fields

--- project/project_template/engine.php
+++ project/project_template/engine.php
@@ -8,6 +8,25 @@
   */
   class <?= Angie_Inflector::camelize($project_name) ?>Engine extends Angie_Engine {
   
+//    /**
+//    * Initialize engine
+//    *
+//    * @param void
+//    * @return null
+//    */
+//    function init() {
+//      if(Angie::getConfig('db.connect_on_init')) {
+//        $hostname = Angie::getConfig('db.hostname');
+//        $username = Angie::getConfig('db.username');
+//        $password = Angie::getConfig('db.password');
+//        $database = Angie::getConfig('db.name');
+//        $persist  = Angie::getConfig('db.persist');
+//        
+//        Doctrine_Manager::connection(new Doctrine_Db("mysql://$username:$password@$hostname/$database"));
+//        Doctrine_Manager::getInstance()->setAttribute(Doctrine::ATTR_VLD, true); // enable validation
+//      } // if
+//    } // init
+  
   } // <?= Angie_Inflector::camelize($project_name) ?>Engine
 
 
--- toys/doctrine/Angie_Doctrine_Record.class.php
+++ toys/doctrine/Angie_Doctrine_Record.class.php
@@ -1,6 +1,60 @@
 <?php
 
+  /**
+  * Doctrine record changed to play well with Angie
+  *
+  * @package Angie.toys
+  * @subpackage doctrine
+  * @author Ilija Studen <ilija.studen@gmail.com>
+  */
   class Angie_Doctrine_Record extends Doctrine_Record {
+    
+    /**
+    * Array of fields that cannot be set using mass set methods
+    *
+    * @var array
+    */
+    private $protected_fields = array();
+    
+    /**
+    * Array of fields that can be set using mass set methods
+    *
+    * @var array
+    */
+    private $allowed_fields = array();
+    
+    /**
+    * Set field values using data from the array
+    * 
+    * If protected fields are defined only fields not in that list will be 
+    * available to be set... Other fields will be skipped.
+    * 
+    * If allowed fields are defined only fields on that list will be set using 
+    * this method. If protecteed fields property is set allowed fields are 
+    * ignored.
+    * 
+    * Any field is considered settable if protected and allowed field lists are 
+    * empty (there is no protected or allowed fields)
+    *
+    * @param array $input
+    * @return null
+    */
+    function setFromArray($input) {
+      if(is_foreachable($input)) {
+        foreach($input as $field_name => $value) {
+          if(count($this->protected_fields)) {
+            if(in_array($this->protected_fields, $field_name)) {
+              continue; // protected field... skip...
+            } // if
+          } elseif(count($this->allowed_fields)) {
+            if(!in_array($field_name, $this->allowed_fields)) {
+              continue; // not allowed field... skip...
+            } // if
+          } // if
+          $this->set($field_name, $value);
+        } // if
+      } // if
+    } // setFromArray
     
     /**
     * Returns true if we have new, unsaved record
@@ -25,6 +79,40 @@
     function isLoaded() {
       return !$this->isNew();
     } // isLoaded
+    
+    /**
+    * Set array of protected fields
+    *
+    * @param void
+    * @return null
+    */
+    function setProtectedFields() {
+      $args = func_get_args();
+      if(is_foreachable($args)) {
+        foreach($args as $arg) {
+          if(!in_array($arg, $this->protected_fields)) {
+            $this->protected_fields[] = $arg;
+          } // if
+        } // foreach
+      } // if
+    } // setProtectedFields
+    
+    /**
+    * Set allowed fields
+    *
+    * @param void
+    * @return null
+    */
+    function setAllowedFields() {
+      $args = func_get_args();
+      if(is_foreachable($args)) {
+        foreach($args as $arg) {
+          if(!in_array($this->allowed_fields)) {
+            $this->allowed_fields[] = $arg;
+          } // if
+        } // foreach
+      } // if
+    } // setAllowedFields
   
   } // Angie_Doctrine_Record
 
